8648dd02a4f0da6b478ffc4b49f78328e22e5e54
upilupal <naufalhanif891@gmail.com>
2024-08-24 21:58:19 +0700

    fix3

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,4 +1,5 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+import authOptions from "@/app/api/auth/[...nextauth]/options";
 import { pusherServer } from "@/app/libs/pusher";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
