e140687df8e33b50a45e7f6fab51f996dab22e2b
upilupal <naufalhanif891@gmail.com>
2024-08-24 21:40:20 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
         swcPlugins: [
             ["next-superjson-plugin", {}]
         ]
     },
-
     images: {
         domains: [
             "res.cloudinary.com",
